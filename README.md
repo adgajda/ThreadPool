de98a5c8c9385c23361fd9401f2ae16727383a69
Adam Gajda <80600850+adgajda@users.noreply.github.com>
2024-03-03 22:20:04 +0100

    Add README.md

--- README.md
+++ README.md
@@ -1,10 +1,31 @@
 # ThreadPool
 
-Example usage in `example.cpp`
+## Example
+
+```cpp
+#include "include/ThreadPool.hpp"
+#include <future>
+#include <iostream>
+
+int main()
+{
+    ThreadPool thread_pool(2);
+    std::future<int> result;
+
+    result = thread_pool.add_task([] { return 3; });
+    std::cout << result.get() << '\n';
+    thread_pool.shutdown();
+
+    return 0;
+}
+```
+
+## Build and Run example
 
 ```
 mkdir build && cd build
 cmake ..
 cmake --build .
 ./example
+3
 ```
--- example.cpp
+++ example.cpp
@@ -1,38 +1,15 @@
 #include "include/ThreadPool.hpp"
-#include <chrono>
 #include <future>
 #include <iostream>
-#include <mutex>
-#include <string_view>
-#include <thread>
-#include <vector>
-
-void print(std::string_view str, int val)
-{
-    static std::mutex mtx;
-    std::lock_guard lock(mtx);
-    std::cout << str << val << '\n';
-}
 
 int main()
 {
     ThreadPool thread_pool(2);
-    std::vector<std::future<int>> results;
+    std::future<int> result;
 
-    for (int task_id = 0; task_id < 8; ++task_id)
-    {
-        results.push_back(thread_pool.add_task([task_id] {
-            print("in function: ", task_id);
-            std::this_thread::sleep_for(std::chrono::seconds(1));
-            return task_id;
-        }));
-    }
-
-    for (auto& result : results)
-    {
-        print("returned value: ", result.get());
-    }
-
+    result = thread_pool.add_task([] { return 3; });
+    std::cout << result.get() << '\n';
     thread_pool.shutdown();
+
     return 0;
 }
